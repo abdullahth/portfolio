d43381c0c1d3dc769809dc0395cfe98e20535af9
abdullahth <abdullahthurwatx@gmail.com>
2021-08-24 01:31:14 +0200

    Models Plugin

---
+++ Backend/plugins/models.js
@@ -0,0 +1,16 @@
+const Product = require('./models/product')
+const package = require('./models/package')
+const Client = require('./models/client')
+const Process = require('./models/process')
+const Offer = require('./models/offer')
+const Order = require('./models/offer')
+
+
+module.exports = {
+    Product: Product,
+    package: package,
+    Client: Client,
+    Process: Process,
+    Offer: Offer,
+    Order: Order,
+}

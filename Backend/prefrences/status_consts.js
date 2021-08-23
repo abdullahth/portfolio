c7d52cf17db0ece43ab77864eec05d75c4cd133e
abdullahth <abdullahthurwatx@gmail.com>
2021-08-24 01:26:12 +0200

    Process Model Consts Status

---
+++ Backend/prefrences/status_consts.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // Planning Stages
+    planning: 0,
+    // Researching Stages
+    researching: 1,
+    // Backend
+    backendDevelopment: 2,
+    // UI/ UX
+    UIUXDesing: 3,
+    // UI Development
+    UIDevelopment: 4,
+    // Components Test
+    testing: 5,
+    // Domain Email etc..
+    extrasImplementation: 6,
+    // Pre-launch test
+    HostTesting: 7,
+    deliveryed: 8,
+    accepted: 9,
+    maintainance: 10,
+    contractClosed: 11,
+}

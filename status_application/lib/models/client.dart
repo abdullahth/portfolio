a3074f23f57ca307988b55b442ee1d77b132ad76
abdullahth <abdullahthurwatx@gmail.com>
2021-08-28 22:50:56 +0200

    Client Model

---
+++ status_application/lib/models/client.dart
@@ -0,0 +1,46 @@
+class Client {
+  late String clientId, productId, packageId;
+
+  late List offers, galleryImagesIds, tasks;
+
+  late int orderStatus;
+
+  late DateTime placment, delivery;
+
+  Client({
+    required this.clientId,
+    required this.productId,
+    required this.packageId,
+    required this.offers,
+    required this.galleryImagesIds,
+    required this.tasks,
+    required this.orderStatus,
+    required this.placment,
+    required this.delivery,
+  });
+
+  Client.fromMap(Map map) {
+    this.clientId = map['clientId'];
+    this.productId = map['productId'];
+    this.packageId = map['packageId'];
+    this.offers = map['offers'];
+    this.galleryImagesIds = map['galleryImagesIds'];
+    this.tasks = map['tasks'];
+    this.orderStatus = map['orderStatus'];
+    this.placment = map['placment'];
+    this.delivery = map['delivery'];
+  }
+
+  
+  Map get map => {
+        'clientId': this.clientId,
+        'productId': this.productId,
+        'packageId': this.packageId,
+        'offers': this.offers,
+        'galleryImagesIds': this.galleryImagesIds,
+        'tasks': this.tasks,
+        'orderStatus': this.orderStatus,
+        'placment': this.placment,
+        'delivery': this.delivery,
+      };
+}

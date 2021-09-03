f05410ccccb44a16c3ddd4cb727ae081df9e53e4
abdullahth <abdullahthurwatx@gmail.com>
2021-09-03 18:07:47 +0200

    Contants

---
+++ website/lib/prefrences/consts.dart
@@ -0,0 +1,12 @@
+final String companyBrief = '''
+An Egyptian company started in January 2020 publishing its first application and Masterpiece Quotmia: For and From Litererians. 
+Now holding business of Egyptian, Newzealandian, Suadi-Arabian Companys and startups.
+
+We're an extension of your team aiming to automate the process making it easier. We're more than a team of marketers and developers. We're the trusted partner for your business.
+
+Our mission is to deliver your business' digital solutions with the highest quality that takes your business to the next level. We help you visualize your work and publish it online on the up-to-date designing and development standards.
+''';
+
+final companyShortBrief = '''
+We specialized in delivering end-to-end Services, Marketing Solutions, Mobile, Web and Desktop Applications for innovation companies throughout the whole middle east.
+''';

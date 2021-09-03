1ad8a3689408154ba714d526cb6c196a942a139f
abdullahth <abdullahthurwatx@gmail.com>
2021-09-03 18:08:04 +0200

    App Themes

---
+++ website/lib/prefrences/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final themeData = ThemeData(
+    primaryColor: Color(0xFF0583f2),
+    accentColor: Color(0xFF989898),
+    fontFamily: 'Quicksand',
+    appBarTheme:
+        AppBarTheme(backgroundColor: Colors.transparent, elevation: 0));
+
+final darkThemeData = ThemeData();

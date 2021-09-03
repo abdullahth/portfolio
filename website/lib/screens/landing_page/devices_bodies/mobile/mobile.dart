15ceca3a775b8fdfd85908382b13580a0d7e07b0
abdullahth <abdullahthurwatx@gmail.com>
2021-09-03 18:10:00 +0200

    Initial Commit

---
+++ website/lib/screens/landing_page/devices_bodies/mobile/mobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobileLandingPage extends StatefulWidget {
+  const MobileLandingPage({Key? key}) : super(key: key);
+
+  @override
+  _MobileLandingPageState createState() => _MobileLandingPageState();
+}
+
+class _MobileLandingPageState extends State<MobileLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ website/lib/screens/landing_page/devices_bodies/tablet/tablet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TabletLandingPage extends StatefulWidget {
+
+  @override
+  _TabletLandingPageState createState() => _TabletLandingPageState();
+}
+
+class _TabletLandingPageState extends State<TabletLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
